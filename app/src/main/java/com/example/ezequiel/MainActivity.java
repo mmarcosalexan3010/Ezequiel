1d03b1d2c2cdcd182d1ba895e3bb5d04e0b916ab
mmarcosalexan3010 <marcosalexan2024@gmail.com>
2025-08-04 19:22:15 -0400

    04/08/2025 19:22

--- app/src/main/java/com/example/ezequiel/MainActivity.java
+++ app/src/main/java/com/example/ezequiel/MainActivity.java
@@ -10,25 +10,5 @@ import android.widget.TextView;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-    EditText editTexto;
-    Button botaoLer;
-    TextView resultadoTexto;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        editTexto = findViewById(R.id.editTexto);
-        botaoLer = findViewById(R.id.botaoLer);
-        resultadoTexto = findViewById(R.id.resultadoTexto);
-
-        botaoLer.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String entrada = editTexto.getText().toString();
-                resultadoTexto.setText("Você digitou: " + entrada);
-            }
-        });
-    }
 }
